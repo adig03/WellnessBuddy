8f65b91689b70d7b6bd886eaec424f2580c82fab
adig03 <149831123+adig03@users.noreply.github.com>
2024-10-29 00:56:17 +0530

    auth ui developed

---
+++ app/src/main/java/com/example/wellnessbuddy/auth/Fragments/ConfirmFragment.kt
@@ -0,0 +1,28 @@
+package com.example.wellnessbuddy.auth.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.wellnessbuddy.R
+
+
+class ConfirmFragment : Fragment(R.layout.fragment_confirm) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirm, container, false)
+    }
+
+
+}
